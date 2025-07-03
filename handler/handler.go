8d322c822f466e65ecd3f66128d3ec66bc39e553
Izabela Ferreira Silva <izabela.silva@utua.com.br>
2025-07-03 15:28:39 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,5 +1,18 @@
 package handler
 
+import (
+	"github.com/IzabelaFerreiraS/go-commerce.git/config"
+	"gorm.io/gorm"
+)
 
+var (
+	logger  *config.Logger
+	db      *gorm.DB
+)
 
+func InitializeHandler(){
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
 
+	logger.Info("Handler initialized")
+}
