aa1bcc3784f86e85332bee0bae8aefc9c22d2fcb
Izabela Ferreira Silva <izabela.silva@utua.com.br>
2025-07-03 13:48:51 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/IzabelaFerreiraS/go-commerce.git/router"
+import (
+	"github.com/IzabelaFerreiraS/go-commerce.git/config"
+	"github.com/IzabelaFerreiraS/go-commerce.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
